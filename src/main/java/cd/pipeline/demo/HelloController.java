5bf29f5310fc2afea3e4fcb4059bec41f8cd255e
lars-gentsch-thb <lars.gentsch@gmail.com>
2025-11-10 10:51:43 +0100

    Added some dump code to be catched by static code analysis.

--- src/main/java/cd/pipeline/demo/HelloController.java
+++ src/main/java/cd/pipeline/demo/HelloController.java
@@ -18,9 +18,34 @@ public class HelloController {
 		return "Greetings from " + name + " " + testValue + "!";
 	}
 
+	public void thisIsDeadCode() {
+		// This is dead code to test code coverage tools
+		int a = 1;
+		int b = 2;
+		int c = a + b;
+		System.out.println(c);
+	}
+
+	@GetMapping("/mailiucous-code")
+	public String mailiucousCode() {
+		// This is another dead code to test code coverage tools
+		for (int i = 0; i < 10; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i + " is even");
+			} else {
+				System.out.println(i + " is odd");
+			}
+		}
+		return "This is mailiucous code";
+	}
 
 	@GetMapping("/something")
 	public ResponseEntity<String> createLogs() {
+				String badFormatatedCode = "This is bad formatated code";
+		if (badFormatatedCode == null) { // useless Null check
+			System.out.println(badFormatatedCode);
+			return ResponseEntity.ok().body("All Ok"); // double return
+		}
 		return ResponseEntity.ok().body("All Ok");
 	}
 }
